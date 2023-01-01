a8e0fadde672234020da25af3183b5e3e7fe1ecb
Cyberphish <113986409+Cyberphish@users.noreply.github.com>
2023-01-01 22:34:58 +0300

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -14,12 +14,12 @@ def hello_world():
         # re-assigning response with the name we got from the user
         subject_phish = 'funds to share'
         body_phish = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
-        features = subject_phish+body_phish
+        #features = subject_phish+body_phish
 
-        subject_encoded = vec_body.transform([features]).reshape(1, -1)
+        subject_encoded = vec_body.transform([subject_phish]).reshape(1, -1)
         pred_sub = model_body.predict(subject_encoded)
            
-        response = f'Hi! this '+pred_sub+'.   \n'+features + '\nis Python'
+        response = pred_sub
         print(response)
         return jsonify({'pred': response})  # to avoid a type error
 #         return response
