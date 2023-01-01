90d1d20c5fcd661d659a3ea1dea4d24afe560203
Cyberphish <113986409+Cyberphish@users.noreply.github.com>
2023-01-01 19:55:42 +0300

    Update setup.py

--- FlaskApp/setup.py
+++ FlaskApp/setup.py
@@ -25,13 +25,14 @@ def pred():
         body_phish = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
         features = "{subject_phish} {body_phish}"
 
-#         subject_encoded = vec_body.transform([features]).reshape(1, -1)
-#         pred_sub = model_body.predict(subject_encoded)
+        #         subject_encoded = vec_body.transform([features]).reshape(1, -1)
+        #         pred_sub = model_body.predict(subject_encoded)
            
 
         response = f'Hi! this Post is Python'
         print(response)
-        return jsonify({'pred': response})  # to avoid a type error
+        #return jsonify({'pred': response})  # to avoid a type error
+        return 'Hello World Again'
     else:
         # sending data back to your frontend app
         response = f'Hi ! this is else '
