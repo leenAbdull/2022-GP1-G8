b6ae601ef4d9252e8aae98caa5dcf6ae2118fc2e
Cyberphish <113986409+Cyberphish@users.noreply.github.com>
2023-01-01 21:42:28 +0300

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import json
 import pickle
 app = Flask(__name__)
+model_body = pickle.load(open('nb.pkl', 'rb'))
+vec_body = pickle.load(open("vec.pkl", "rb"))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,10 +15,10 @@ def hello_world():
         body_phish = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
         features = subject_phish+body_phish
 
-        #subject_encoded = vec_body.transform([features]).reshape(1, -1)
-        #pred_sub = model_body.predict(subject_encoded)
+        subject_encoded = vec_body.transform([features]).reshape(1, -1)
+        pred_sub = model_body.predict(subject_encoded)
            
-        response = f'Hi! this'+features+ 'is Python'
+        response = f'Hi! this '+pred_sub+'.   \n'+features + '\nis Python'
         print(response)
         return jsonify({'pred': response})  # to avoid a type error
 #         return response
