ccf4bb32abb4c3ff91a6880b905087f14423cb0c
Cyberphish <113986409+Cyberphish@users.noreply.github.com>
2023-01-10 14:16:47 +0300

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -31,13 +31,19 @@ def pred():
         bag_of_words = pd.DataFrame(
                 encode, columns=vec.get_feature_names_out())
 
-        # features_encoded = vec.transform([encode]).reshape(1, -1)
         prediction = model.predict(encode)
+        Vocab_list = {}
 
-        # response = f"Hi! this Post "+pred_sub+"is Python"
-        response = f'{prediction}'
-        print(response)
-        return jsonify({'pred': response})  # to avoid a type error
+        for vector in bag_of_words:
+           if (bag_of_words[vector].values > 0):
+               Vocab_list[bag_of_words[vector].name] = bag_of_words[vector].values[0]
+
+        prediction = f'{prediction}'
+        response = f'{Vocab_list}'        
+        print(response)       
+        return jsonify({'prediction': prediction[1],
+                        'vocabulary': response
+                        }) 
     else:
         # sending data back to your frontend app
         subject = 'funds to share'
@@ -60,32 +66,3 @@ def pred():
         return jsonify({'prediction': prediction[1],
                         'vocabulary': response
                         })
-
-
-'''
-from flask import Flask, request, jsonify
-import json
-import pickle
-import sklearn
-app = Flask(__name__)
-model_body = pickle.load(open('nb.pkl', 'rb'))
-vec_body = pickle.load(open("vec.pkl", "rb"))
-
-
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    if (request.method == 'GET'):
-        print('inn')
-        # re-assigning response with the name we got from the user
-        subject_phish = 'funds to share'
-        body_phish = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
-        
-        features = " ".join([subject_phish, body_phish]) #subject_phish+body_phish
-
-        subject_encoded = vec_body.transform([features]).reshape(1, -1)
-        pred_sub = model_body.predict(subject_encoded)
-           
-        response = pred_sub
-        return jsonify({'pred': 'response' })
-#         return 'respone {response}'
-'''
