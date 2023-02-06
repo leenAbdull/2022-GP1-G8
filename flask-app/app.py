3c1989f7f7f3023cbcc6945d711b892c723822e7
Cyberphish <113986409+Cyberphish@users.noreply.github.com>
2023-02-06 19:28:16 +0300

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,8 +5,7 @@ import pickle
 import sklearn
 import pandas as pd
 model = pickle.load(open('nb.pkl', 'rb'))
-# vec_body = pickle.load(open("vec.pkl", "rb"))
-vec = pickle.load(open("bag_words.pkl", "rb"))
+vec = pickle.load(open("vec.pkl", "rb"))
 
 
 
@@ -26,7 +25,30 @@ def pred():
         request_data = json.loads(data.decode('utf-8'))
         subject = request_data['subject']
         body = request_data['body']
-        features = " ".join([subject, body])
+        features = "\n".join([subject, body])
+        encode = vec.transform([features]).toarray()
+        bag_of_words = pd.DataFrame(
+               encode, columns=vec.get_feature_names_out())
+
+        prediction = model.predict(encode)
+        Vocab_list = {}
+
+        for vector in bag_of_words:
+           if (bag_of_words[vector].values > 0):
+               Vocab_list[bag_of_words[vector].name] = bag_of_words[vector].values[0]
+
+        prediction = f'{prediction}'
+        response = f'{Vocab_list}'        
+        return jsonify({'prediction': prediction[1],
+                        'vocabulary': response
+                        }) 
+
+    else:
+        # sending data back to your frontend app
+        subject = 'funds to share'
+        body = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
+        Vocab_list = {}
+       features = " ".join([subject, body])
         encode = vec.transform([features]).toarray()
         bag_of_words = pd.DataFrame(
                 encode, columns=vec.get_feature_names_out())
@@ -43,29 +65,3 @@ def pred():
         return jsonify({'prediction': prediction[1],
                         'vocabulary': response
                         }) 
-
-#         prediction = f'{prediction}'
-#         return jsonify({'prediction': prediction[1]
-#                         }) 
-    else:
-        # sending data back to your frontend app
-        subject = 'funds to share'
-        body = "The University of Washington System is sharing funds for all students during this pandemic, please update your \n financial aid status to claim yours. \nLogin.uw.edu/covid-19-aid-update\n For instructions on Accepting Your Financial Aid on https://login.uw.edu/login/login./.\n Regards,\n Assistant Professor \nUniversity of Washington"
-        Vocab_list = {}
-        features = " ".join([subject, body])
-        encode = vec.transform([features]).toarray()
-        bag_of_words = pd.DataFrame(
-                encode, columns=vec.get_feature_names_out())
-
-        prediction = model.predict(encode)
-        
-        for vector in bag_of_words:
-           if (bag_of_words[vector].values > 0):
-               Vocab_list[bag_of_words[vector].name] = bag_of_words[vector].values[0]
-
-        prediction = f'{prediction}'
-        response = f'{Vocab_list}'        
-        print(response)       
-        return jsonify({'prediction': prediction[1],
-                        'vocabulary': response
-                        })
